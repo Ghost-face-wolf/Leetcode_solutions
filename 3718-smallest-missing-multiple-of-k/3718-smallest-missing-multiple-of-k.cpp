3fe84aa328b09de52b1fb433001249f6c34ecdd8
Ghost-face-wolf <dogravishal67@gmail.com>
2026-08-25 12:23:27 +0530

    Time: 4 ms (23.25%), Space: 25 MB (45.06%) - LeetHub

---
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int missingMultiple(vector<int>& nums, int k) {
+        unordered_set<int> numset(nums.begin(),nums.end());
+        int multiple=k;
+        while(numset.count(multiple)){
+            multiple+=k;
+        }
+        return multiple;
+    }
+};
